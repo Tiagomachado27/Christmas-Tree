ec71fce179d0d8a73f30eebe20372a404b0e6d2c
Tiagomachado27 <43068863+Tiagomachado27@users.noreply.github.com>
2020-03-31 04:23:48 +0100

    Add files via upload

---
+++ Tree.py
@@ -0,0 +1,13 @@
+line_num = 0
+num_ast = 1
+space_num = 0
+
+line_num = space_num = int(input("Escolhe um número: "))
+
+
+while space_num != 0:
+    print(" "*int(space_num) + "*"*num_ast)
+    space_num-= 1
+    num_ast += 2
+    line_num += 1
+print ("Merry Christmas!")
